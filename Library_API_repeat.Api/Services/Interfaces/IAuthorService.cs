3c48c1f034c4b30ec81e4e5cd205f106b7619787
eljes <d00251881@student.dkit.ie>
2026-08-24 10:19:37 -0400

    creating interfaces

---
+++ Library_API_repeat.Api/Services/Interfaces/IAuthorService.cs
@@ -0,0 +1,12 @@
+﻿using Library_API_repeat.Api.DTOs.Authors;
+namespace Library_API_repeat.Api.Services.Interfaces
+{
+    public interface IAuthorService
+    {
+        Task<IEnumerable<AuthorDTO>> GetAllAsync();
+        Task<AuthorDTO?> GetByIdAsync(int id);
+        Task<AuthorDTO> CreateAsync(CreateAuthorDTO dto);
+        Task<bool> UpdateAsync(int id, UpdateAuthorDTO dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
