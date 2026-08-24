31f072c9f1971dba396d9ec4f04651cfbd6eba43
eljes <d00251881@student.dkit.ie>
2026-08-24 10:02:02 -0400

    populated Loan dto

--- Library_API_repeat.Api/DTOs/Loans/LoanDTO.cs
+++ Library_API_repeat.Api/DTOs/Loans/LoanDTO.cs
@@ -1,6 +1,22 @@
-﻿namespace Library_API_repeat.Api.DTOs.Loans
+﻿using Library_API_repeat.Api.Models;
+
+namespace Library_API_repeat.Api.DTOs.Loans
 {
     public class LoanDTO
     {
+        public int id { get; set; }
+        public int BookId { get; set; }
+
+        public string BookTitle { get; set; } = string.Empty;
+
+        public int MemberId { get; set; }
+
+        public string MemberName { get; set; } = string.Empty;
+
+        public DateTime LoanDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public DateTime? ReturnDate { get; set; }
     }
 }
