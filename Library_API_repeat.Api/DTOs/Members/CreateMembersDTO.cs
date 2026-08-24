9f95fb07f3b1313dc026af6563d691c32ad79fd0
eljes <d00251881@student.dkit.ie>
2026-08-24 09:57:12 -0400

    populated create member dto

--- Library_API_repeat.Api/DTOs/Members/CreateMembersDTO.cs
+++ Library_API_repeat.Api/DTOs/Members/CreateMembersDTO.cs
@@ -2,5 +2,11 @@
 {
     public class CreateMembersDTO
     {
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public DateTime MembershipDate { get; set; }
     }
 }
