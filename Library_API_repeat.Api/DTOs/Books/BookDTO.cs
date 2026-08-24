19f22f0f1120b7e40e7574934bd9a004e9748f6b
eljes <d00251881@student.dkit.ie>
2026-08-24 09:53:50 -0400

    populated Book DTO

--- Library_API_repeat.Api/DTOs/Books/BookDTO.cs
+++ Library_API_repeat.Api/DTOs/Books/BookDTO.cs
@@ -1,6 +1,22 @@
-﻿namespace Library_API_repeat.Api.DTOs.Books
+﻿using Library_API_repeat.Api.Models;
+
+namespace Library_API_repeat.Api.DTOs.Books
 {
     public class BookDTO
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public string ISBN { get; set; } = string.Empty;
+
+        public int PublicationYear { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public string AuthorName { get; set; } = string.Empty;
+
+        
     }
 }
