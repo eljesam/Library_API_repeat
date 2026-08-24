349a8f19e4c4b78d7fc9f4b78e7848ca9b93cc04
eljesam <124920260+eljesam@users.noreply.github.com>
2026-08-24 05:23:15 -0400

    created API and solution Folders. Creating database and tables

---
+++ Library_API_repeat/Library_API_repeat.Api/Models/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace Library_API_repeat.Api.Models
+{
+    public class Author
+    {
+        public int it {  get; set; }
+        public string name { get; set; } = string.Empty;
+        public string? Country { get; set; }
+
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+
+    }
+}
---
+++ Library_API_repeat/Library_API_repeat.Api/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace Library_API_repeat.Api.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public string ISBN { get; set; } = string.Empty;
+
+        public int PublicationYear { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+
+        public int AuthorId { get; set; }
+
+        public Author? Author { get; set; }
+
+        public ICollection<Loan> Loans { get; set; } = new List<Loan>();
+    }
+}
