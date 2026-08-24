5712bf62f630af967fe5655fdd1efb3cb68178eb
eljes <d00251881@student.dkit.ie>
2026-08-24 06:31:19 -0400

    CRUD for Authors

---
+++ Library_API_repeat.Api/Controllers/AuthorsController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Library_API_repeat.Api.Data;
+using Library_API_repeat.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_API_repeat.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly LibraryDbContext _context;
+
+        public AuthorsController(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get: api/authors
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            return await _context.Authors.ToListAsync();
+        }
+
+        //Get: api/authors/5
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Author>> GetAuthor(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return author;
+        }
+
+        //POST: api/authors
+        [HttpPost]
+        public async Task<ActionResult<Author>> CreateAuthor(Author author)
+        {
+            _context.Authors.Add(author);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetAuthor),
+                new { id = author.id },
+                author);
+        }
+
+        //PUT: api/authors/5
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> UpdateAuthor(int id, Author author)
+        {
+            if (id != author.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(author).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuthorExists(id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        //Delete: api/authors/5
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _context.Authors.Remove(author);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+    private bool AuthorExists(int id)
+        {
+            return _context.Authors.Any(e => e.id == id);
+        }
+    }
+}
