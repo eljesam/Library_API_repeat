71826a26fc6f8fbbc67332d159b61803b5ad053b
eljes <d00251881@student.dkit.ie>
2026-08-24 09:49:16 -0400

    populating Author DTO

--- Library_API_repeat.Api/DTOs/Authors/AuthorDTO.cs
+++ Library_API_repeat.Api/DTOs/Authors/AuthorDTO.cs
@@ -2,5 +2,8 @@
 {
     public class AuthorDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Country { get; set; }
     }
 }
