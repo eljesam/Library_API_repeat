6bc704e37c369cc82dde146a964413d230f91b25
eljes <d00251881@student.dkit.ie>
2026-08-24 09:56:34 -0400

    populated Membe dto

--- Library_API_repeat.Api/DTOs/Members/MembersDTO.cs
+++ Library_API_repeat.Api/DTOs/Members/MembersDTO.cs
@@ -2,5 +2,12 @@
 {
     public class MembersDTO
     {
+        public int id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public DateTime MembershipDate { get; set; }
     }
 }
