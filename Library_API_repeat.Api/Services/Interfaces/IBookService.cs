ef30750ac6b4ddf54d9bae67f8eaf0803c7e417c
eljes <d00251881@student.dkit.ie>
2026-08-25 10:49:45 -0400

    created book interface

---
+++ Library_API_repeat.Api/Services/Interfaces/IBookService.cs
@@ -0,0 +1,19 @@
+﻿using Library_API_repeat.Api.DTOs.Books;
+
+namespace Library_API_repeat.Api.Services.Interfaces
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<BookDTO>> GetAllAsync();
+
+        Task<BookDTO?> GetByIdAsync(int id);
+
+        Task<BookDTO?> CreateAsync(CreateBookDTO dto);
+
+        Task<bool> UpdateAsync(int id, UpdateBookDTO dto);
+
+        Task<bool> DeleteAsync(int id);
+
+        Task<bool> AuthorExistsAsync(int authorId);
+    }
+}
