64ed7ffdb3116d5ef77ca5f60c815416c1818a87
eljes <d00251881@student.dkit.ie>
2026-08-27 11:35:17 -0400

    Added the books page

---
+++ Library_API_repeat.Client/Models/Books/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Library_API_repeat.Client.Models.Books
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string ISBN { get; set; } = string.Empty;
+
+        public int PublicationYear { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public string AuthorName { get; set; } = string.Empty;
+    }
+}
---
+++ Library_API_repeat.Client/Models/Books/CreateBookRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_API_repeat.Client.Models.Books
+{
+    public class CreateBookRequest
+    {
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string ISBN { get; set; } = string.Empty;
+
+        [Range(1, 3000)]
+        public int PublicationYear { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please select an author.")]
+        public int AuthorId { get; set; }
+    }
+}
